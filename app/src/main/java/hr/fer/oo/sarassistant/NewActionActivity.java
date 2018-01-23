5275cefa45d0353b375f64b9f08fcb3c650b8028
creationspirit <creationspirit@users.noreply.github.com>
2018-01-23 15:24:08 +0100

    incijalno

---
+++ app/src/main/java/hr/fer/oo/sarassistant/NewActionActivity.java
@@ -0,0 +1,13 @@
+package hr.fer.oo.sarassistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewActionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_action);
+    }
+}
